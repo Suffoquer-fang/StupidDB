a94860dcf2b50e4eb5fb053a96f49036f6a9ac02
suffoquer <1161290791@qq.com>
2020-11-20 23:56:24 +0800

    add sm/create

--- src/sm/create.cpp
+++ src/sm/create.cpp
@@ -1,1 +1,21 @@
-#include "cstdio"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		cerr << "Usage: " << argv[0] << " DBName\n";
+		return 1;
+	}
+	char command[100] = "mkdir ";
+	system(strcat(command, argv[1]));
+	chdir(argv[1]);
+
+	ofstream outfile;
+	outfile.open("meta.db");
+	outfile << '0' << endl;
+    outfile.close();
+	return 0;
+}
